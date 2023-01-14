b365d1cbed32db6902ee1b5ba1db6ab68cc18ed0
berkayzincir <86130570+berkayzincir@users.noreply.github.com>
2023-01-14 21:15:55 +0300

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ import axios from "axios";
 async function getData (Number) {
 const {data: user} = await axios("https://jsonplaceholder.typicode.com/users/" + Number);
 const {data: post} = await axios("https://jsonplaceholder.typicode.com/posts?userId=" + Number)
-const data = [user,post[0]]
+const data = [user,post]
 console.log(data)
 
 
